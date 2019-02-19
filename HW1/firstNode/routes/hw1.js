0dc8920222eb44653d3af9139e2b5bcb3b154975
Asutosh <asupat12@gmail.com>
2019-02-19 09:58:59 -0500

    Update get/post methods

--- HW1/firstNode/routes/hw1.js
+++ HW1/firstNode/routes/hw1.js
@@ -3,12 +3,12 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/get', function(req, res, next) {
-  res.send({string:"Hey now"})
+  res.json({string:"Hey now"})
 });
 
 // POST method route
 router.post('/post', function (req, res) {
-  var user_name = req.param('name');
-  res.send("Hey " + user_name);
+  let user_name = req.body.name;
+  res.json({string:`${user_name}`});
 })
 module.exports = router;

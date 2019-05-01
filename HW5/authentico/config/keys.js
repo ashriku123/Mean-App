e1b98f2c3513a0364a229ad909737f066a02a463
Asutosh <asupat12@gmail.com>
2019-05-01 19:59:29 -0400

    Add HW5

---
+++ HW5/authentico/config/keys.js
@@ -0,0 +1,13 @@
+// add this file to .gitignore
+module.exports = {
+        google: {
+            clientID: '1016883774945-rj3f5qd73qhlp18vb746hp05eem0gqge.apps.googleusercontent.com',
+            clientSecret: 'uVpk2u2XZ2h2ahqfF5EVbtTJ'
+        },
+        mongodb: {
+            dbURI: 'mongodb://localhost:27017/authentico'
+        },
+        session: {
+            cookieKey: 'asutosh'
+        }
+};

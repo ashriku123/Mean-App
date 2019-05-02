b0e5450a23b01d6d3eaac6f64dbb14e57df3a8bd
Asutosh <asupat12@gmail.com>
2019-05-01 20:07:43 -0400

    Remove keys

--- HW5/authentico/config/keys.js
+++ HW5/authentico/config/keys.js
@@ -1,8 +1,8 @@
 // add this file to .gitignore
 module.exports = {
         google: {
-            clientID: '1016883774945-rj3f5qd73qhlp18vb746hp05eem0gqge.apps.googleusercontent.com',
-            clientSecret: 'uVpk2u2XZ2h2ahqfF5EVbtTJ'
+            clientID: '',
+            clientSecret: ''
         },
         mongodb: {
             dbURI: 'mongodb://localhost:27017/authentico'

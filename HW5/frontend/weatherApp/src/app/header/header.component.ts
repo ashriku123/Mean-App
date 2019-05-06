bdbc2855fb5392227794047d7c7871dfd1427217
Asutosh <asupat12@gmail.com>
2019-05-06 19:47:28 -0400

    .

--- HW5/frontend/weatherApp/src/app/header/header.component.ts
+++ HW5/frontend/weatherApp/src/app/header/header.component.ts
@@ -25,7 +25,6 @@ export class HeaderComponent implements OnInit, OnDestroy{
   onLogin() {
     this.http.get('http://localhost:3000/auth/google', { withCredentials: true })
       .subscribe((postData) => {
-        console.log(postData);
         this.authService.login();
       });
   }
@@ -33,7 +32,6 @@ export class HeaderComponent implements OnInit, OnDestroy{
   onLogout() {
     this.http.get('http://localhost:3000/auth/logout', { withCredentials: true })
     .subscribe((postData) => {
-      console.log(postData);
       this.authService.logout();
     });
   }
